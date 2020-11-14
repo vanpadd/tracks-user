cb00f499e27a950849c8e5c71f54809024f37b0d
Waldo van Paddenburg <waldovp@gmail.com>
2020-11-14 20:16:16 +1300

    implements SignupScreen layout

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -1,15 +1,51 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, StyleSheet } from "react-native";
+import { Text, Input, Button } from "react-native-elements";
+import Spacer from "../components/Spacer";
 
 const SignupScreen = ({ navigation: { navigate } }) => {
   return (
-    <View>
-      <Text style={{ fontSize: 48 }}>SignupScreen</Text>
-      <Button title="go to Signin" onPress={() => navigate("Signin")} />
+    <View style={styles.containerStyle}>
+      <Spacer>
+        <Text h3>Sign up to Tracker</Text>
+      </Spacer>
+      <Spacer />
+      <Input
+        label="Your Email Address"
+        placeholder="email@address.com"
+        leftIconContainerStyle={styles.leftIconStyle}
+        leftIcon={{ type: "MaterialCommunityIcons", name: "email" }}
+      />
+      <Input
+        label="Password"
+        placeholder="Password"
+        secureTextEntry={true}
+        leftIconContainerStyle={styles.leftIconStyle}
+        leftIcon={{ type: "MaterialCommunityIcons", name: "lock" }}
+      />
+      <Spacer />
+      <Button title="Sign Up" type="solid" buttonStyle={styles.buttonStyle} />
     </View>
   );
 };
 
-const styles = StyleSheet.create({});
+SignupScreen.navigationOptions = () => {
+  return { headershow: false };
+};
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 200,
+  },
+  leftIconStyle: {
+    opacity: 0.3,
+  },
+  buttonStyle: {
+    paddingHorizontal: 100,
+  },
+});
 
 export default SignupScreen;
