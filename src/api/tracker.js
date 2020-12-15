2b84ac5f7699bf709ad8df3a016fd09d7586522a
Waldo van Paddenburg <waldovp@gmail.com>
2020-12-15 21:31:23 +1300

    adds MapView and Polyline

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://15207829dc32.ngrok.io',
+  baseURL: 'http://177416630f19.ngrok.io',
 });
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,12 +1,44 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import MapView, { Polyline } from 'react-native-maps';
 
 const Map = () => {
+  const points = [];
+  for (let i = 0; i < 20; i++) {
+    if (i % 2 === 0) {
+      points.push({
+        latitude: -36.848461 + i * 0.001,
+        longitude: 174.763336 + i * 0.001,
+      });
+    } else {
+      points.push({
+        latitude: -36.848461 + i * 0.002,
+        longitude: 174.763336 + i * 0.001,
+      });
+    }
+  }
+
   return (
     <View>
-      <Text>Map</Text>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: -36.848461,
+          longitude: 174.763336,
+          latitudeDelta: 0.01,
+          longitudeDelta: 0.01,
+        }}
+      >
+        <Polyline coordinates={points} />
+      </MapView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  map: {
+    height: 300,
+  },
+});
 
 export default Map;
