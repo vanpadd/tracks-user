31c85274aa673e54fc708bf896cfe474e2374ad3
Waldo van Paddenburg <waldovp@gmail.com>
2020-12-16 22:12:58 +1300

    implement request permission for geolocation

--- src/screens/TrackCreateScreen.js
+++ src/screens/TrackCreateScreen.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 import { Text } from 'react-native-elements';
 import { SafeAreaView } from 'react-navigation';
 import Map from '../components/Map';
+import * as Location from 'expo-location';
 
 const TrackCreateScreen = () => {
+  const [errorMsg, setErrorMsg] = useState(null);
+
+  const startWatching = async () => {
+    let { granted } = await Location.requestPermissionsAsync();
+    if (!granted) {
+      setErrorMsg('Permission to access location was denied');
+      return;
+    }
+  };
+
+  useEffect(() => {
+    startWatching();
+  }, []);
+
   return (
     <SafeAreaView forceInset={{ top: 'always' }}>
       <Text h2>TrackCreateScreen</Text>
